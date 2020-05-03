ea05cb71479c85ef25af801c171edcad86f1933a
Emmet Allen <59685332+Mrskillful@users.noreply.github.com>
2020-05-03 01:53:24 -0400

    Golf.js ver 1.0
    Just a simple golf score recorder using basic JS.

---
+++ GolfScore.js
@@ -0,0 +1,34 @@
+// User Inputs Par and Stroke
+
+golfScore(5, 4);
+
+// Function passes users golfScore input and returns output
+
+function golfScore(par, strokes) { 
+    if (strokes == 1){
+      return "Hole-in-one!";
+    }
+      else if(strokes <= par - 2){
+        return "Eagle";
+      }
+        else if(strokes == par - 1){
+          return "Birdie";
+        }
+          else if(strokes == par){
+            return "Par";
+          }
+            else if(strokes == par + 1){
+              return "Bogey";
+            }
+              else if(strokes == par + 2){
+                return "Double Bogey";
+              }
+                else if(strokes >= par + 3){
+                  return "Go Home!"
+                }
+                  else{
+                    return "Change Me";
+                  }
+  }
+
+
