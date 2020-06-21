c9affbdf69c1ad16efb2bda7ce7912b015f2b26b
Emmet Allen <59685332+Mrskillful@users.noreply.github.com>
2020-06-21 19:45:48 -0400

    Create Bool_Who.js
    I don't feel like I should have notes for this, but I'm adding it anyway.

---
+++ Bool_Who.js
@@ -0,0 +1,13 @@
+function booWho(bool) {
+ 
+  //If bool is true or bool is false
+  if (bool === true || bool === false){
+    // Return true
+    return true;
+  }
+    else{
+      return false;
+    }
+}
+
+booWho(null);
